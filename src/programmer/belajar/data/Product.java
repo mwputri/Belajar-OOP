ef022c4502b92ba06232461408036146b555cc4b
mwputri <mwputrimeiliniahp@gmail.com>
2024-07-11 12:00:01 +0700

    create package

---
+++ src/programmer/belajar/data/Product.java
@@ -0,0 +1,11 @@
+package programmer.belajar.data;
+
+class Product {
+    String name;
+    int price;
+
+    Product(String name, int price){
+        this.name = name;
+        this.price = price; 
+    }
+}
